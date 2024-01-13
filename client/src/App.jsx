ebf18ad6563068b6e2b67851dfb0d5fac6e03f47
govind <joshigovind740@gmail.com>
2024-01-13 23:29:33 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,29 @@
-
-import { Route, Routes } from 'react-router-dom'
-import Layout from './Layout'
-import IndexPage from './pages/IndexPage'
-import LoginPage from './pages/LoginPage.jsx'
-import RegisterPage from './pages/RegisterPage'
-import axios from 'axios'
-import { UserContextProvider } from './UserContext'
-import Account from './pages/Account.jsx'
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage";
+import axios from "axios";
+import { UserContextProvider } from "./UserContext";
+import Account from "./pages/Account.jsx";
 
 function App() {
-  axios.defaults.baseURL = 'http://127.0.0.1:8080';
-  axios.defaults.withCredentials =true
+  axios.defaults.baseURL = "http://127.0.0.1:8080";
+  axios.defaults.withCredentials = true;
 
   return (
     <UserContextProvider>
-
-    <Routes>
-      <Route path='/' element={<Layout/>}>
-      <Route index element={<IndexPage/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/register' element={<RegisterPage/>}/>
-      <Route path='/account' element={<Account/>}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account/" element={<Account />} />
+          <Route path="/account/:subPage" element={<Account />} />
+        </Route>
+      </Routes>
     </UserContextProvider>
-     
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../UserContext';
-import { Link, Navigate } from 'react-router-dom';
+import { Link, Navigate, useParams } from 'react-router-dom';
 
 export default function Account() {
     const { ready, user } = useContext(UserContext);
@@ -10,12 +10,26 @@ export default function Account() {
     if (ready && !user ) {
         return <Navigate to={'/login'}/>
     }
+
+    let { subPage } = useParams();
+  if (subPage !== undefined) {
+      subPage = "profile";
+    }
+  function linkClass(type) {
+    let classes = 'py-2 px-6';
+    if (type === subPage) {
+      classes += 'bg-primary text-white rounded-full';
+    }
+    return classes;
+
+  }
+
   return (
     <div>
           <nav className='w-full flex justify-center mt-8 gap-2'>
-              <Link className='p-2 px-6 bg-primary text-white rounded-full ' to={'/account/booking'}>My Profile</Link>
-              <Link className='p-2 px-6' to={'/account/booking'}>My Booking</Link>
-              <Link className='p-2 px-6' to={'/account/places'}>My Accomodation</Link>
+              <Link className={linkClass('profile')} to={'/account'}>My Profile</Link>
+              <Link className={linkClass('booking')} to={'/account/booking'}>My Booking</Link>
+              <Link className={linkClass('places')} to={'/account/places'}>My Accomodation</Link>
               
         </nav>
     </div>
